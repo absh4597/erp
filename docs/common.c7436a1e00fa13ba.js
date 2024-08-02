32c4f3f852a7d8681ebefc91585516b12842be67
abhishek singh <abhishek.singh@kreatetechnologies.com>
2024-08-02 10:30:41 +0530

    changes

---
+++ docs/common.c7436a1e00fa13ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkadminportal=self.webpackChunkadminportal||[]).push([[592],{145:(l,d,c)=>{c.d(d,{m:()=>u});var u=(()=>{return(e=u||(u={}))[e.create=1]="create",e[e.update=2]="update",e[e.delete=3]="delete",e[e.view=4]="view",u;var e})()}}]);
